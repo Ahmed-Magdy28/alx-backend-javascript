6f7c9779b3f5c114b91d1e317e568a3105171d4d
Ahmed-Magdy28 <ray2legand@gmail.com>
2024-07-22 22:47:08 +0300

    task 12

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,21 @@
 module.exports = {
   env: {
-    browser: false,
+    browser: true,
     es6: true,
     jest: true,
   },
-  extends: ["airbnb-base", "plugin:jest/all"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["jest"],
-  rules: {
-    "no-console": "off",
-    "no-shadow": "off",
-    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
-  },
-  overrides: [
-    {
-      files: ["*.js"],
-      excludedFiles: "babel.config.js",
-    },
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: [
+    'jest',
+  ],
+  rules: {
+    // Add your custom rules here
+  },
 };
