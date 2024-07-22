1a71baa7284cd0b5b901251b1a99b6b7c659818b
Ahmed-Magdy28 <ray2legand@gmail.com>
2024-07-22 15:11:44 +0300

    Task 3 fix0

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,2 +1,2 @@
-const  getSumOfHoods = (initialNumber, expansion1989, expansion2019) => {expansion1989 = expansion1989 === undefined ? 89 : expansion1989;expansion2019 = expansion2019 === undefined ? 19 : expansion2019;return initialNumber + expansion1989 + expansion2019;};
-export default getSumOfHoods;
+// 3. Default parameter
+const  getSumOfHoods = (initialNumber, expansion1989, expansion2019) => {expansion1989 = expansion1989 === undefined ? 89 : expansion1989;expansion2019 = expansion2019 === undefined ? 19 : expansion2019;return initialNumber + expansion1989 + expansion2019;};export default getSumOfHoods;
