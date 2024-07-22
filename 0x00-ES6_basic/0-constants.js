d3bcd8d47706df0f8970eec794812b3b9f53931e
Ahmed-Magdy28 <ray2legand@gmail.com>
2024-07-22 23:04:26 +0300

    task 0 fix 2

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,15 @@
-// task.js
-
-// This function returns a constant string
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// This function returns a string to be appended
 export function getLast() {
   return ' is okay';
 }
 
-// This function combines two strings and returns the result
 export function taskNext() {
-  const combination = 'But sometimes let';
-  return combination + getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
